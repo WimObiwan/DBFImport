4d032761139fe80afc8ac4c371bbc92969f6eec8
Wim Devos <wim@obiwan.be>
2018-09-20 19:34:31 +0200

    Inititial commit

---
+++ DBFImport/DBFImporter.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DBFImport
+{
+    class DbfHeader
+    {
+        public byte Version { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public int RecordCount { get; set; }
+        public short HeaderLength { get; set; }
+        public short RecordLength { get; set; }
+
+        public int FieldCount => (HeaderLength / 32 - 1);
+    }
+
+    class DbfFieldDescriptor
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        public char TypeChar { get; set; }
+        public int Length { get; set; }
+        public byte DecimalCount { get; set; }
+
+        public string GetSqlDataType()
+        {
+            switch (TypeChar)
+            {
+                case 'C':
+                    return $"VARCHAR({Length})";
+                case 'I':
+                    return "INT";
+                case 'N':
+                    return $"DECIMAL({Length + 1}, {DecimalCount})";
+                case 'L':
+                    return "BIT";
+                case 'D':
+                    return "DATETIME";
+                case 'M':
+                    return "VARCHAR(MAX)";
+                case 'T':
+                    return "DATETIME";
+                case 'W': //?
+                    return "VARCHAR(MAX)";
+                case '0':
+                    return "INT";
+                case 'G':
+                    return "VARCHAR(MAX)";
+                case 'F':
+                    return "FLOAT";
+                default:
+                    throw new NotSupportedException();
+            }
+
+        }
+
+        internal SqlParameter GetSqlParameter(string name)
+        {
+            switch (TypeChar)
+            {
+                case 'C':
+                    return new SqlParameter(name, SqlDbType.VarChar, Length);
+                case 'I':
+                    return new SqlParameter(name, SqlDbType.Int);
+                case 'N':
+                {
+                    var par = new SqlParameter(name, SqlDbType.Decimal);
+                    par.Precision = (byte)(Length + 3);
+                    par.Scale = DecimalCount;
+                    return par;
+                }
+                case 'L':
+                    return new SqlParameter(name, SqlDbType.Bit);
+                case 'D':
+                    return new SqlParameter(name, SqlDbType.DateTime);
+                case 'M':
+                    return new SqlParameter(name, SqlDbType.VarChar, -1);
+                case 'T':
+                    return new SqlParameter(name, SqlDbType.DateTime);
+                case 'W': //?
+                    return new SqlParameter(name, SqlDbType.VarChar, -1);
+                case '0':
+                    return new SqlParameter(name, SqlDbType.Int);
+                case 'G':
+                    return new SqlParameter(name, SqlDbType.VarChar, -1);
+                case 'F':
+                    return new SqlParameter(name, SqlDbType.Float);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+
+    class DbfRecord
+    {
+        public int RecordNo { get; set; }
+        public bool Deleted { get; set; }
+        public object[] Fields { get; set; }
+    }
+
+    class DbfFileStream : IDisposable
+    {
+        private FileStream fileStream;
+        private BinaryReader binaryReader;
+        private int recordNo;
+
+        public DbfHeader Header { get; }
+
+        public IReadOnlyList<DbfFieldDescriptor> FieldDescriptors { get; }
+
+        public IEnumerable<DbfRecord> Records
+        {
+            get
+            {
+                for (int recordNo = 0; recordNo < Header.RecordCount; recordNo++) {
+                    yield return ReadRecord(recordNo, FieldDescriptors);
+                }
+            }
+        }
+
+        public DbfFileStream(string filename)
+        {
+            fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            binaryReader = new BinaryReader(fileStream);
+
+            try {
+                Header = ReadHeader(binaryReader);
+            } catch (Exception e) {
+                throw new Exception("Failed to read header", e);
+            }
+
+            var fieldDescriptors = new List<DbfFieldDescriptor>();
+            try
+            {
+                int no = 0;
+                while (true)
+                {
+                    var fieldDescriptor = ReadFieldDescriptor(binaryReader, no++);
+                    if (fieldDescriptor == null)
+                        break;
+
+                    fieldDescriptors.Add(fieldDescriptor);
+                }
+            } catch (Exception e) {
+                throw new Exception("Failed to read field descriptors", e);
+            }
+
+            // Read remainder of header
+
+            int bytesRead = 32 + (32 * fieldDescriptors.Count) + 1;
+            binaryReader.ReadBytes(Header.HeaderLength - bytesRead);
+
+            FieldDescriptors = fieldDescriptors;
+        }
+
+        public void Dispose()
+        {
+            if (binaryReader != null)
+            {
+                binaryReader.Dispose();
+                binaryReader = null;
+            }
+
+            if (fileStream != null)
+            {
+                fileStream.Dispose();
+                fileStream = null;
+            }
+        }
+
+        const string Reserved = "Reserved";
+
+        void ReadReservedByte(string message)
+        {
+            ReadReservedByte(0, message);
+        }
+
+        void ReadReservedByte(byte expected = 0, string message = null)
+        {
+            byte reserved = binaryReader.ReadByte();
+            if (reserved != expected)
+                throw new NotSupportedException($"{message ?? "Reserved"} ({reserved} instead of {expected})");
+        }
+
+        void ReadReservedBytes(int count, string message)
+        {
+            ReadReservedBytes(count, 0, message);
+        }
+
+        void ReadReservedBytes(int count, byte expected = 0, string message = null)
+        {
+            byte[] reserved = binaryReader.ReadBytes(count);
+            if (reserved.Length < count)
+                throw new NotSupportedException(
+                    $"{message ?? Reserved} ({reserved.Length} bytes read instead of {count})");
+
+            for (int i = 0; i < count; i++)
+            {
+                if (reserved[i] != expected)
+                    throw new NotSupportedException(
+                        $"{message ?? Reserved} (byte {i} of {count} is {reserved} instead of {expected})");
+            }
+        }
+
+        DbfHeader ReadHeader(BinaryReader br)
+        {
+            var header = new DbfHeader();
+
+            // Version
+            header.Version = br.ReadByte();
+
+            // YYMMDD
+            int yy = br.ReadByte();
+            yy += (yy < 70) ? 2000 : 1900;
+            int mm = br.ReadByte();
+            int dd = br.ReadByte();
+            header.LastUpdate = new DateTime(yy, mm, dd);
+
+            // Records
+            header.RecordCount = br.ReadInt32();
+
+            header.HeaderLength = br.ReadInt16();
+
+            header.RecordLength = br.ReadInt16();
+
+            ReadReservedByte();
+            ReadReservedByte();
+            ReadReservedByte("Incomplete transaction");
+            ReadReservedByte("Encryption");
+            ReadReservedBytes(12);
+            br.ReadByte(); // production mdx
+            br.ReadByte(); // language driver id
+            ReadReservedByte();
+            ReadReservedByte();
+
+            return header;
+        }
+
+        DbfFieldDescriptor ReadFieldDescriptor(BinaryReader br, int fdNo)
+        {
+            var fieldDescriptor = new DbfFieldDescriptor();
+            fieldDescriptor.No = fdNo;
+
+            try
+            {
+                var fieldNameBytes = new byte[11];
+                fieldNameBytes[0] = br.ReadByte();
+                if (fieldNameBytes[0] == 0x0D)
+                    return null; // 0x0D means end of field descriptor list
+
+                br.Read(fieldNameBytes, 1, 10);
+                fieldDescriptor.Name = System.Text.Encoding.ASCII.GetString(fieldNameBytes).TrimEnd('\0');
+                fieldDescriptor.TypeChar = (char) br.ReadByte();
+                br.ReadByte(); // reserved
+                br.ReadByte(); // reserved
+                br.ReadByte(); // reserved
+                br.ReadByte(); // reserved
+                fieldDescriptor.Length = br.ReadByte();
+                fieldDescriptor.DecimalCount = br.ReadByte();
+                br.ReadBytes(2); // work area id
+                br.ReadByte(); // example
+                br.ReadBytes(10); // reserved
+                br.ReadByte(); // production mdx
+
+                return fieldDescriptor;
+            }
+            catch (Exception e)
+            {
+                if (string.IsNullOrWhiteSpace(fieldDescriptor.Name))
+                    throw new Exception($"Failed to read field descriptor #{fdNo + 1}", e);
+                else
+                    throw new Exception($"Failed to read field descriptor #{fdNo + 1} ({fieldDescriptor.Name})", e);
+            }
+        }
+
+        private DbfRecord ReadRecord(int recordNo, IReadOnlyList<DbfFieldDescriptor> fieldDescriptors)
+        {
+            try
+            {
+                var record = new DbfRecord();
+
+                record.RecordNo = recordNo;
+
+                byte status = binaryReader.ReadByte();
+                if (status != 0x20 && status != 0x2A)
+                    throw new NotSupportedException($"Unknown record status ({status})");
+                record.Deleted = (status == 0x2A);
+
+                record.Fields = new object[fieldDescriptors.Count];
+
+                for (int fdNo = 0; fdNo < fieldDescriptors.Count; fdNo++)
+                {
+                    var fd = fieldDescriptors[fdNo];
+                    try
+                    {
+                        record.Fields[fdNo] = ReadField(fd);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception($"Failed to parse column #{fd.No} ({fd.Name})", e);
+                    }
+                }
+
+                return record;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to read record #{recordNo}", e);
+            }
+        }
+
+        private object ReadField(DbfFieldDescriptor fd)
+        {
+            var data = binaryReader.ReadBytes(fd.Length);
+            switch (fd.TypeChar)
+            {
+                case 'C':
+                    return ReadFieldText(data);
+                case 'I':
+                    return ReadFieldInteger(data);
+                case 'N':
+                    return ReadFieldNumeric(data);
+                case 'L':
+                    return ReadFieldLogical(data);
+                case 'D':
+                    return ReadFieldDate(data);
+                case 'M':
+                    return ReadFieldMemo(data);
+                case 'T':
+                    return ReadFieldTime(data);
+                case 'W': //?
+                    return null;
+                case '0'
+                    : // _NullFlags?  https://stackoverflow.com/questions/30886730/adding-data-to-dbf-file-adds-column-nullflags
+                    return null;
+            }
+
+            throw new NotSupportedException($"Field type ({fd.TypeChar})");
+        }
+
+        private DateTime? ReadFieldTime(byte[] data)
+        {
+            if (data.Length != 8)
+                throw new InvalidOperationException($"Time has invalid length ({data.Length} instead of 8)");
+
+            // "Days since Jan 1, 4713 BC"
+            // JD = Days since "12h Jan 1, 4713 BC"
+            // 19/09/2018 JD=2458380.5 ==> Days = 2458381
+            // DateTime(19/09/2018) - DateTime.MinValue = 736955
+            // "Days since Jan 1, 4713 BC" on DateTime.MinValue = 2458381 - 736955 = 1721426
+
+            const int daysOnDateTimeMinValue = 1721426;
+
+            int days = (data[0] << 0) | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+            int msecs = (data[4] << 0) | (data[5] << 8) | (data[6] << 16) | (data[7] << 24);
+
+            if (days == 0 && msecs == 0)
+                return null;
+
+            return DateTime.MinValue.AddDays(days - daysOnDateTimeMinValue).AddMilliseconds(msecs);
+        }
+
+        private object ReadFieldMemo(byte[] data)
+        {
+            if (data.Length == 4)
+            {
+                if (data[0] == 0 && data[1] == 0 && data[2] == 0 && data[3] == 0)
+                    return null; //???
+                if (data[0] == 0x20 && data[1] == 0x20 && data[2] == 0x20 && data[3] == 0x20)
+                    return null; //???
+
+                return "<Not implemented>";
+            }
+
+            throw new NotSupportedException($"Field type Memo with length <> 4");
+        }
+
+        private DateTime? ReadFieldDate(byte[] data)
+        {
+            var text = System.Text.Encoding.ASCII.GetString(data);
+            if (text == "        ")
+                return null;
+
+            if (text.StartsWith("00"))
+            {
+                text = "20" + text.Substring(2);
+            }
+
+            return DateTime.ParseExact(text, "yyyyMMdd", null);
+        }
+
+        private bool? ReadFieldLogical(byte[] data)
+        {
+            if (data.Length != 1)
+                throw new InvalidOperationException($"Logical has invalid length ({data.Length} instead of 1)");
+
+            var ch = (char) data[0];
+            if (ch == 'Y' || ch == 'y' || ch == 'T' || ch == 't')
+                return true;
+
+            if (ch == 'N' || ch == 'n' || ch == 'F' || ch == 'f')
+                return false;
+
+            if (ch == '?' || ch == ' ')
+                return null;
+
+            throw new NotSupportedException($"Unknown logical value ({ch})");
+        }
+
+        private decimal? ReadFieldNumeric(byte[] data)
+        {
+            string text = System.Text.Encoding.ASCII.GetString(data).TrimStart();
+
+            if (text == "")
+                return null;
+
+            if (text.StartsWith('.'))
+                text = '0' + text;
+
+            var d = decimal.Parse(text, CultureInfo.InvariantCulture.NumberFormat);
+            return d;
+        }
+
+        private int ReadFieldInteger(byte[] data)
+        {
+            if (data.Length != 4)
+                throw new InvalidOperationException($"Integer has invalid length ({data.Length} instead of 4)");
+
+            return data[0] << 0 | data[1] << 8 | data[2] << 16 | data[3] << 24;
+        }
+
+        private string ReadFieldText(byte[] data)
+        {
+            return System.Text.Encoding.ASCII.GetString(data).TrimEnd();
+        }
+
+    }
+}
---
+++ DBFImport/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+
+namespace DBFImport
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("DBFImporter 1.0");
+
+            if (args.Length < 2 || args.Length > 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: DBFImporter.exe [path] [server] [database]");
+                Console.WriteLine("Usage: DBFImporter.exe [path] [conn]");
+                Console.WriteLine();
+                Console.WriteLine("   path      file path, directory path or file mask of the DBF file(s)");
+                Console.WriteLine("   server    SQL Server hostname or IP address (optionally including instance name)");
+                Console.WriteLine("   database  name of existing database");
+                Console.WriteLine("   conn      valid connection string");
+                Console.WriteLine();
+                Console.WriteLine(@"Example: DBFImporter.exe ""c:\My DBF files\*.DBF"" DEVSERVER\SQL2017 ImportedDbfFiles");
+                return 1;
+            }
+
+            string path = args[0];
+            string connectionString;
+            if (args.Length == 3)
+            {
+                string server = args[1];
+                string database = args[2];
+
+                System.Data.SqlClient.SqlConnectionStringBuilder connectionStringBuilder =
+                    new SqlConnectionStringBuilder();
+                connectionStringBuilder.DataSource = server;
+                connectionStringBuilder.InitialCatalog = database;
+                connectionStringBuilder.IntegratedSecurity = true;
+                connectionString = connectionStringBuilder.ConnectionString;
+            }
+            else
+            {
+                connectionString = args[1];
+            }
+
+            int failedFiles = 0;
+            int succeededFiles = 0;
+            Stopwatch sw = Stopwatch.StartNew();
+
+            if (File.Exists(path))
+            {
+                if (ProcessFile(path, connectionString))
+                    succeededFiles++;
+                else
+                    failedFiles++;
+            }
+            else
+            {
+                string mask;
+                if (Directory.Exists(path))
+                {
+                    mask = "*.DBF";
+                }
+                else
+                {
+                    mask = Path.GetFileName(path);
+                    path = Path.GetDirectoryName(path);
+                }
+
+                foreach (var file in Directory.EnumerateFiles(path, mask))
+                {
+                    if (ProcessFile(file, connectionString))
+                        succeededFiles++;
+                    else
+                        failedFiles++;
+                }
+            }
+
+            Console.WriteLine($"Succeeded files: {succeededFiles}");
+            Console.WriteLine($"Failed files:    {failedFiles}");
+            Console.WriteLine($"Total Duration:  {sw.Elapsed}");
+
+            return failedFiles;
+        }
+
+        static bool ProcessFile(string filename, string connectionString)
+        {
+            Console.WriteLine($"Processing {filename}...");
+            Stopwatch sw = Stopwatch.StartNew();
+            bool result;
+            try
+            {
+                using (DbfFileStream dbfFileStream = new DbfFileStream(filename))
+                {
+                    string table = Path.GetFileNameWithoutExtension(filename);
+
+                    Console.WriteLine($"  LastUpdate: {dbfFileStream.Header.LastUpdate.ToShortDateString()}");
+                    Console.WriteLine($"  Fields:     {dbfFileStream.Header.FieldCount}");
+                    Console.WriteLine($"  Records:    {dbfFileStream.Header.RecordCount}");
+
+                    int insertCount = CreateTable(connectionString, table, dbfFileStream.FieldDescriptors, dbfFileStream.Records);
+                    Console.WriteLine($"  Inserted:   {insertCount}");
+                }
+
+                result = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to process file {filename}");
+                Console.WriteLine($"   Exception: {e.Message}");
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    Console.WriteLine($"       Inner: {e.Message}");
+                }
+
+#if DEBUG
+                throw;
+#endif
+
+                result = false;
+            }
+
+            Console.WriteLine($"  Duration:   {sw.Elapsed}");
+
+            return result;
+        }
+
+        private static int CreateTable(string connectionString, string table, IReadOnlyList<DbfFieldDescriptor> fieldDescriptors,
+            IEnumerable<DbfRecord> records)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Failed to connect to database", e);
+                }
+
+                try
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = $"IF OBJECT_ID('{table}', 'U') IS NOT NULL DROP TABLE [{table}];";
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Failed to drop existing table ${table}", e);
+                }
+
+                try
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine($"CREATE TABLE [{table}] (");
+                        bool first = true;
+                        foreach (var fieldDescriptor in fieldDescriptors)
+                        {
+                            if (first)
+                                first = false;
+                            else
+                                sb.Append(", ");
+                            sb.AppendLine($"[{fieldDescriptor.Name}] {fieldDescriptor.GetSqlDataType()}");
+                        }
+                        sb.AppendLine($")");
+                        cmd.CommandText = sb.ToString();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Failed to create table ${table}", e);
+                }
+
+                try
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine($"INSERT INTO [{table}] (");
+                        bool first = true;
+                        foreach (var fieldDescriptor in fieldDescriptors)
+                        {
+                            if (first)
+                                first = false;
+                            else
+                                sb.Append(", ");
+                            sb.Append($"[{fieldDescriptor.Name}]");
+                        }
+
+                        sb.AppendLine($") VALUES (");
+                        int no = 0;
+                        first = true;
+                        foreach (var fieldDescriptor in fieldDescriptors)
+                        {
+                            if (first)
+                                first = false;
+                            else
+                                sb.Append(", ");
+                            sb.Append($"@p{no}");
+
+                            cmd.Parameters.Add(fieldDescriptor.GetSqlParameter($"@p{no}"));
+
+                            no++;
+                        }
+
+                        sb.AppendLine($")");
+                        cmd.CommandText = sb.ToString();
+                        int insertCount = 0;
+                        foreach (var record in records)
+                        {
+                            if (record.Deleted)
+                                continue;
+
+                            try
+                            {
+                                no = 0;
+                                foreach (var field in record.Fields)
+                                {
+                                    cmd.Parameters[$"@p{no}"].Value = field ?? DBNull.Value;
+                                    no++;
+                                }
+
+                                cmd.ExecuteNonQuery();
+                                insertCount++;
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception($"Failed to insert record #{record.RecordNo + 1} into database, {insertCount} already inserted", e);
+                            }
+                        }
+
+                        return insertCount;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Failed to fill table ${table}", e);
+                }
+            }
+        }
+    }
+}
